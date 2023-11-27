4a6bf96a8c6092e47fda7f3883e584c14a258a23
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-27 10:24:55 +1000

    flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,1 +1,13 @@
 # Install a package
+
+package {'flask':
+    ensure   => '2.1.0',
+    name     => 'flask',
+    provider => 'pip3',
+}
+
+package {'Werkzeug':
+    ensure   => '2.1.1',
+    name     => 'Werkzeug',
+    provider => 'pip3',
+}
