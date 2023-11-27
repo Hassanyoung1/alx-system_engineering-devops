7ad7e98fb22ea88178aa1426dd297470fdc02d0f
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-27 12:32:46 +1000

    exec

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,8 +1,15 @@
-# Kills a process named killmenow
+# Create a Puppet manifest file, e.g., killmenow.pp
 
-exec { 'killmenow':
-  command     => 'pkill -f killmenow',
-  path        => ['/bin', '/usr/bin'],
-  onlyif      => 'pgrep -f killmenow',
-  refreshonly => true,
+# Define a class that contains the exec resource
+class killmenow {
+  # Use the exec resource to run the pkill command
+  exec { 'killmenow_process':
+    command     => 'pkill killmenow',
+    path        => '/bin:/usr/bin', # Add any necessary paths
+    refreshonly => true,             # Only run when notified
+    logoutput   => true,             # Log the command output
+  }
 }
+
+# Include the class to ensure it is applied
+include killmenow
