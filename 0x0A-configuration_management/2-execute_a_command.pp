abe2b5a3de736e10bb01d5572a65625fae4a6de5
Hassanyoung1 <hhassanhakeem@gmail.com>
2023-11-27 12:26:25 +1000

    PUPPET

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,11 +1,8 @@
-# Execute a command
+# Kills a process named killmenow
 
 exec { 'killmenow':
-    command     => 'pkill -f killmenow',
-    path        => ['/bin:/usr/bin'],
-    onlyif      => 'pgrep -f killnow',
-    refreshonly => true,
-    logoutput   => true,
+  command     => 'pkill -f killmenow',
+  path        => ['/bin', '/usr/bin'],
+  onlyif      => 'pgrep -f killmenow',
+  refreshonly => true,
 }
-
-
