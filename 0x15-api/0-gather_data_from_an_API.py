6ee75890d30f346a65e95460bae8e7afdfcc9b3d
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-04 11:32:23 +1000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A Python script that, for a given employee ID,
+"""
+A Python script that, for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
