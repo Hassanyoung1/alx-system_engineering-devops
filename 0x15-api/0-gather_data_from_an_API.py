e3184eaf700e91d3345755ce4d051876de81d4f3
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-04 10:55:37 +1000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""A Python script that, for a given employee ID,
-returns infomation about his/her TODO list progress.
-"""
+"""Return info about employee's TODO list progress"""
 
-if __name__ == '__main__':
-    import requests
-    import sys
+import requests
+from sys import argv
 
-    if len(sys.argv) != 2:
-        print(f'Usage: {sys.argv[0]} <user_id>', file=sys.stderr)
-        exit(1)
+if __name__ == "__main__":
+    baseUrl = 'https://jsonplaceholder.typicode.com/users'
+    fullUrl = f"{baseUrl}/{argv[1]}"
 
-    user_id = sys.argv[1]
-    url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    name = requests.get(url).json().get('name')
+    user = requests.get(fullUrl).json()
+    name = user.get('name')
 
-    url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
-    tasks = requests.get(url).json()
-    done = 0
-    tasks_done = []
-    for task in tasks:
-        if task.get('completed'):
-            tasks_done.append(task)
-            done += 1
+    todosUrl = f"{fullUrl}/todos"
+    todos = requests.get(todosUrl).json()
 
-    print(f'Employee {name} is done with tasks({done}/{len(tasks)}):')
-    for task in tasks_done:
-        print(f'\t {task.get("title")}')
+    prompt = f"Employee {name} is done with"
+    completed = [todo.get('title')
+                 for todo in todos if todo.get('completed') is True]
+    print(f"{prompt} tasks({len(completed)}/{len(todos)}):")
+    [print(f"\t {task}") for task in completed]
