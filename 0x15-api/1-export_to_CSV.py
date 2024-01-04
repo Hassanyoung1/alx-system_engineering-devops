9e2b14c2786a4afc0b547af0b12f030c0caddb29
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-04 12:01:32 +1000

    Export

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3
-"""Return info about employee's TODO list progress"""
+"""A Python script that, for a given employee ID,
+returns information about his/her TODO list progress.
+"""
 
-import requests
-from sys import argv
+if __name__ == '__main__':
+    import requests
+    import sys
+    import csv
 
-if __name__ == "__main__":
-    baseUrl = 'https://jsonplaceholder.typicode.com/users'
-    fullUrl = f"{baseUrl}/{argv[1]}"
+    if len(sys.argv) != 2:
+        print(f'Usage: {sys.argv[0]} <user_id>', file=sys.stderr)
+        exit(1)
 
-    user = requests.get(fullUrl).json()
-    name = user.get('username')
+    user_id = sys.argv[1]
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    name = requests.get(url).json().get('name')
 
-    user_id = argv[1]
-    todosUrl = f"{fullUrl}/todos"
-    todos = requests.get(todosUrl).json()
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+    tasks = requests.get(url).json()
+    done = 0
+    tasks_done = []
+    for task in tasks:
+        if task.get('completed'):
+            tasks_done.append(task)
+            done += 1
 
-    with open(f"{user_id}.csv", 'w') as csv_file:
-        for todo in todos:
-            csv_file.write(f'"{user_id}","{name}","{todo.get("completed")}",'
-                           f'"{todo.get("title")}"\n')
+    print(f'Employee {name} is done with tasks({done}/{len(tasks)}):')
+
+    csv_file_name = f'{user_id}.csv'
+    with open(csv_file_name, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(
+            ["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+        for task in tasks_done:
+            writer.writerow([user_id, name, task.get(
+                "completed"), task.get("title")])
+
+    print(f'Data exported to {csv_file_name} successfully.')
