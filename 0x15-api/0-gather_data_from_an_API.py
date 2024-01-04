9b4f53911aa7ec8af26614ccaf7a988a34a0411e
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-04 10:52:39 +1000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,41 +1,29 @@
 #!/usr/bin/python3
-
-"""
-A Python script that, using REST API, for a given employee ID, returns
-information about his/her TODO list progress.
+"""A Python script that, for a given employee ID,
+returns infomation about his/her TODO list progress.
 """
 
 if __name__ == '__main__':
     import requests
     import sys
 
-    """
-    Usage: python3 gather_data_from_an_API.py <user_id>
-    """
     if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <user_id>", sys.stderr)
+        print(f'Usage: {sys.argv[0]} <user_id>', file=sys.stderr)
         exit(1)
 
     user_id = sys.argv[1]
-    baseUrl = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    employee_name = requests.get(baseUrl).json().get('name')
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
+    name = requests.get(url).json().get('name')
 
-    todosUrl = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-    todos = requests.get(todosUrl).json()
+    url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
+    tasks = requests.get(url).json()
     done = 0
-    tasks = []
-
-    """
-    Loop through the todos and count the completed tasks
-    """
-    for todo in todos:
-        if todo.get('completed'):
-            tasks.append(todo.get('title'))
+    tasks_done = []
+    for task in tasks:
+        if task.get('completed'):
+            tasks_done.append(task)
             done += 1
 
-    """
-    Print the progress of the employee's tasks
-    """
-    print(f"Employee {employee_name} is done with tasks({done}/{len(tasks)}):")
-    for task in tasks:
-        print("\t{}".format(task))
+    print(f'Employee {name} is done with tasks({done}/{len(tasks)}):')
+    for task in tasks_done:
+        print(f'\t {task.get("title")}')
