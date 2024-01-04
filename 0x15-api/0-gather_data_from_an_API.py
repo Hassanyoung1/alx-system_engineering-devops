d5b6d55ce688e131256319b9bcb466db114de074
Hassanyoung1 <hhassanhakeem@gmail.com>
2024-01-04 11:31:28 +1000

    Gather data from an API

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-
-"""
-A Python script that, using REST API, for a given employee ID, returns
-information about his/her TODO list progress.
+"""A Python script that, for a given employee ID,
+returns information about his/her TODO list progress.
 """
 
 if __name__ == '__main__':
@@ -14,31 +12,32 @@ if __name__ == '__main__':
     """
 
     if len(sys.argv) != 2:
-        print(f"Usage: {sys.argv[0]} <user_id>", sys.stderr)
+        print(f'Usage: {sys.argv[0]} <user_id>', file=sys.stderr)
         exit(1)
 
-    user_id = sys.argv[1]
-    baseUrl = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    employee_name = requests.get(baseUrl).json().get('name')
+    employee_id = sys.argv[1]
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+    employee_name = requests.get(user_url).json().get('name')
 
-    todosUrl = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
-    todos = requests.get(todosUrl).json()
-    done = 0
-    total = 0
-    tasks = []
+    todos_url = f'https: // jsonplaceholder.typicode.com / users / {
+        employee_id} / todos'
+    todos = requests.get(todos_url).json()
+    completed_count = 0
+    completed_tasks = []
 
     """
-    Loop through the todos and count the completed tasks
+    Loop through the tasks and count the completed tasks
     """
-    for todo in todos:
-        total += 1
-        if todo.get('completed'):
-            done += 1
-            tasks.append(todo.get('title'))
+    for task in todos:
+        if task.get('completed'):
+            completed_tasks.append(task)
+            completed_count += 1
 
     """
     Print the progress of the employee's tasks
     """
-    print(f'Employee {employee_name} is done with tasks({done}/{len(tasks)}):')
-    for task in tasks:
+    print(
+        f'Employee {employee_name} is done with tasks({completed_count} / {
+            len(todos)}): ')
+    for task in completed_tasks:
         print(f'\t {task.get("title")}')
